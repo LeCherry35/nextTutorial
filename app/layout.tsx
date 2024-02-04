183c60e0fd0976cb9e7f0db062fbba04573793ac
Cherry <lenyamm35@gmail.com>
2024-02-04 13:53:40 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
+  
 }) {
+;
+  
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body  className={`${inter.className} antialiased`}>
+        <h1>Pisya koroleva!</h1>
+        {children}
+        </body>
     </html>
   );
 }
